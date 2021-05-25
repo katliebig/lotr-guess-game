383e6e98cd36b784d7c8cfd7e78e44cde77d1dff
katliebig <liebig.kat@gmail.com>
2021-05-25 14:21:57 +0200

    updated api key

--- code/script.js
+++ code/script.js
@@ -1,4 +1,4 @@
-const bearerToken = "xiBTA2j96-n4Bkl7SBY-"
+const bearerToken = "K_j-rZJXREyooJ4mT2KP"
 
 const restartContainer = document.querySelector("#restart-container")
 const rightGuessesContainer = document.querySelector("#right-guesses")
